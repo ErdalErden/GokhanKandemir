f733320d5e99c8833b3aa1e1543c30b12e8bd5bb
ErdalErden <you@example.com>
2022-04-24 23:06:12 +0300

    çalışıldı

---
+++ VueJs-Bootcamp/1 - vue-instance/app.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            title : "VueJS Bootcamp 1.gün",
+            content : "Lorem ipsum dolor sit amet ...",
+        };
+    },
+}).mount("#app");
